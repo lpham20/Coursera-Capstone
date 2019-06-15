98bb217078b2b6afed588068910a49c6479c4a89
lpham20 <51814327+lpham20@users.noreply.github.com>
2019-06-15 21:12:58 +0700

    Add files via upload

---
+++ Capstone+project.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# ## this notebook will be mainly used for the capstone project
+
+# In[8]:
+
+import pandas as pd
+
+
+# In[7]:
+
+import numpy as np
+
+
+# In[9]:
+
+print("Hello Capstone Project")
+
+
+# In[ ]:
+
+
+
